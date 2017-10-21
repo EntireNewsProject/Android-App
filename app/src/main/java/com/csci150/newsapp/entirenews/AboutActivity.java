e1095bb71a38acc34742ca245a88b06f039e6490
kishanshah <kishanshah147@gmail.com>
2017-10-21 13:21:04 -0700

    Update about activity.

--- app/src/main/java/com/csci150/newsapp/entirenews/AboutActivity.java
+++ app/src/main/java/com/csci150/newsapp/entirenews/AboutActivity.java
@@ -2,12 +2,56 @@ package com.csci150.newsapp.entirenews;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Toast;
+
+import mehdi.sakout.aboutpage.AboutPage;
+import mehdi.sakout.aboutpage.Element;
 
 public class AboutActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_about);
+        //setContentView(R.layout.activity_about);
+
+        Element adsElement = new Element();
+        adsElement.setTitle("Advertise here");
+
+        View aboutPage = new AboutPage(this)
+                .isRTL(false)
+                .setImage(R.drawable.smaplelogo)
+                .setDescription("This is demo version")
+                .addItem(new Element().setTitle("Version 1.0"))
+                .addItem(adsElement)
+                .addGroup("Libraries we use")
+                .addGroup("Private Policy")
+                .addGroup("Connect")
+                .addGitHub("Entire News Project (News App)")
+                .addPlayStore("My Playstore")
+                .addEmail("kishanshah147@mail.fresnostate.edu")
+                .addItem(createCopyright())
+                .create();
+
+        setContentView(aboutPage);
     }
+
+    private Element createCopyright() {
+        Element copyright = new Element();
+        final String copyrightString = String.format("Copyright %d by EntireNews", java.util.Calendar.getInstance().get(java.util.Calendar.YEAR));
+        copyright.setTitle(copyrightString);
+        copyright.setIconDrawable(R.mipmap.ic_launcher);
+        copyright.setGravity(Gravity.CENTER);
+        copyright.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(AboutActivity.this, copyrightString, Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        return copyright;
+
+    }
+
 }
