5c66e68811e729658c55d50a8fbbdcfc45c17a4d
Shifat Denocyte <thunderroid@gmail.com>
2017-10-28 14:41:41 -0700

    Initialized Saved Activity

--- app/src/main/java/com/csci150/newsapp/entirenews/SavedActivity.java
+++ app/src/main/java/com/csci150/newsapp/entirenews/SavedActivity.java
@@ -2,12 +2,49 @@ package com.csci150.newsapp.entirenews;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.RecyclerView;
+
+import com.csci150.newsapp.entirenews.utils.Utils;
+
+import java.util.List;
 
 public class SavedActivity extends Activity {
+    private final String TAG = "SavedActivity";
+
+    private int mPage = 1;
+    private List<NewsItem> mItems;
+
+    private RecyclerView mRecyclerView;
+    private CoordinatorLayout mCoordinatorLayout;
+    private SwipeRefreshLayout mSwipeRefreshLayout;
+
+    private NewsItemAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Utils.print(TAG, "onCreate()");
         setContentView(R.layout.activity_saved);
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Utils.print(TAG, "onResume()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Utils.print(TAG, "onPause()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Utils.print(TAG, "onDestroy()");
+    }
+
 }
