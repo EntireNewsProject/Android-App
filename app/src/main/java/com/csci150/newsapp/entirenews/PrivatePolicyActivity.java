704b8b172065ff8c981e77a15a78bcbe5906e050
kishanshah <kishanshah147@gmail.com>
2017-10-29 23:22:09 -0700

    Changes in the About page

---
+++ app/src/main/java/com/csci150/newsapp/entirenews/PrivatePolicyActivity.java
@@ -0,0 +1,17 @@
+package com.csci150.newsapp.entirenews;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PrivatePolicyActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_private_policy);
+    }
+}
+
+
