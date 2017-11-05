f9752979f6ae3e221ee910995343ee66f341420c
kishanshah <kishanshah147@gmail.com>
2017-11-05 13:32:00 -0800

    About/Private Policy Bullet

--- app/src/main/java/com/csci150/newsapp/entirenews/AboutActivity.java
+++ app/src/main/java/com/csci150/newsapp/entirenews/AboutActivity.java
@@ -1,5 +1,7 @@
 package com.csci150.newsapp.entirenews;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Gravity;
@@ -54,9 +56,6 @@ public class AboutActivity extends AppCompatActivity {
         });
         return copyright;
     }
-
-
-
-    }
+}
 
 
