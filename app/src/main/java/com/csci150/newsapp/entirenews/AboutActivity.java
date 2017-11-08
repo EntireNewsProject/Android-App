ba7b8bd57d763fdc64546d66a92146c51ec43467
kishanshah <kishanshah147@gmail.com>
2017-11-07 17:13:09 -0800

    About/privatePolicyActivity and LibActivity functional button added.

--- app/src/main/java/com/csci150/newsapp/entirenews/AboutActivity.java
+++ app/src/main/java/com/csci150/newsapp/entirenews/AboutActivity.java
@@ -1,9 +1,12 @@
 package com.csci150.newsapp.entirenews;
 
+import android.app.ActivityOptions;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.util.Pair;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Toast;
@@ -11,7 +14,7 @@ import android.widget.Toast;
 import mehdi.sakout.aboutpage.AboutPage;
 import mehdi.sakout.aboutpage.Element;
 
-public class AboutActivity extends AppCompatActivity {
+public class AboutActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +29,29 @@ public class AboutActivity extends AppCompatActivity {
                 .setImage(R.drawable.smaplelogo)
                 .setDescription("This is demo version")
                 .addItem(new Element().setTitle("Version 1.0"))
-                .addItem(new Element().setTitle("Private Policy"))
-                .addItem(new Element().setTitle("Libraries we use"))
+                .addItem(new Element().setTitle("Private Policy").setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                        Log.d("Kishan","Its working");
+                        Intent intent = new Intent();
+                        intent.setClass(getApplicationContext(), PrivatePolicyActivity.class);
+
+                        startActivityForResult(intent, 100);
+
+                    }
+                }))
+                .addItem(new Element().setTitle("Libraries we use").setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Log.d("Kishan","Its working");
+                        Intent intent = new Intent();
+                        intent.setClass(getApplicationContext(), LibActivity.class);
+
+                        startActivityForResult(intent, 100);
+
+                    }
+                }))
                 .addItem(adsElement)
                 .addGroup("Connect with us")
                 .addEmail("kishanshah147@mail.fresnostate.edu")
@@ -38,6 +62,8 @@ public class AboutActivity extends AppCompatActivity {
 
         setContentView(aboutPage);
     }
+
+
 
 
 
