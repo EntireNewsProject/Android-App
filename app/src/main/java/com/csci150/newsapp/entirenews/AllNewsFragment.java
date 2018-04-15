e6eccbb1e4bd6caa2bd17592b1fb1ff77a5bec18
Shifat Denocyte <thunderroid@gmail.com>
2018-04-15 14:18:11 -0700

    Created new Fragment

---
+++ app/src/main/java/com/csci150/newsapp/entirenews/AllNewsFragment.java
@@ -0,0 +1,44 @@
+package com.csci150.newsapp.entirenews;
+
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link AllNewsFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class AllNewsFragment extends Fragment {
+
+    public AllNewsFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @return A new instance of fragment AllNewsFragment.
+     */
+    public static AllNewsFragment newInstance() {
+        return new AllNewsFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_all_news, container, false);
+    }
+
+}
--- app/src/main/java/com/csci150/newsapp/entirenews/BottomNavActivity.java
+++ app/src/main/java/com/csci150/newsapp/entirenews/BottomNavActivity.java
@@ -9,7 +9,10 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 
+import com.csci150.newsapp.entirenews.utils.Utils;
+
 public class BottomNavActivity extends AppCompatActivity {
+    private final String TAG = "BottomNavActivity";
 
     private Fragment fragment;
     private FragmentManager fragmentManager;
@@ -20,20 +23,20 @@ public class BottomNavActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_all_news:
-
+                    fragment = AllNewsFragment.newInstance();
                     break;
                 case R.id.navigation_recommended:
-
+                    fragment = AllNewsFragment.newInstance();
                     break;
                 case R.id.navigation_trending:
-
+                    fragment = AllNewsFragment.newInstance();
                     break;
                 case R.id.navigation_saved:
-
+                    fragment = AllNewsFragment.newInstance();
                     break;
             }
             final FragmentTransaction transaction = fragmentManager.beginTransaction();
-            transaction.replace(R.id.main_container, fragment).commit();
+            transaction.replace(R.id.main_container, fragment, null).commit();
             return true;
         }
     };
@@ -48,5 +51,12 @@ public class BottomNavActivity extends AppCompatActivity {
         BottomNavigationView navigation = findViewById(R.id.navigation);
         BottomNavigationViewHelper.removeShiftMode(navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        if (savedInstanceState == null) {
+            Utils.print(TAG, "savedInstanceState == null");
+            final FragmentTransaction transaction = fragmentManager.beginTransaction();
+            transaction.replace(R.id.main_container, AllNewsFragment.newInstance(), null)
+                    .commit();
+        }
     }
 }
