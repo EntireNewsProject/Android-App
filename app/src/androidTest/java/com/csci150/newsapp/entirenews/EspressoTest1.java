12163359c8714bb41b3139905f2895745b0e304a
Shifat Denocyte <thunderroid@gmail.com>
2018-04-30 09:45:54 -0700

    First Espresso Test

---
+++ app/src/androidTest/java/com/csci150/newsapp/entirenews/EspressoTest1.java
@@ -0,0 +1,48 @@
+package com.csci150.newsapp.entirenews;
+
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.FixMethodOrder;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+/**
+ * Created by Shifatul Islam (Denocyte) on 4/30/2018 9:14 AM.
+ * A listing app, where you can find everything in one place.
+ */
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class EspressoTest1 {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(
+            MainActivity.class);
+
+
+    @Before
+    public void setUp() throws Exception {
+        //Before Test case execution
+    }
+
+    @Test
+    public void test1ChatId() {
+        onView(withText("Entire News")).check(matches(isDisplayed()));
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        //After Test case Execution
+    }
+}
